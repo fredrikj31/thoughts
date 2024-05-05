b14e472df7f76ebe3ba072726248b54dd367dfde
Fredrik Johansen <fredrik1206@gmail.com>
2024-05-05 09:35:50 +0200

    feat(app): adds sample shadcn button to app

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@shadcn-ui/components/ui/button";
+
 export const App = () => {
   return (
     <>
       <h1 className="text-3xl font-bold underline">Hello World</h1>
+      <Button>Hello</Button>
     </>
   );
 };
