ed9ce972d78e6bdf5b1e842d83d8ba84bec8d550
Fredrik Johansen <fredrik1206@gmail.com>
2024-05-05 09:42:09 +0200

    feat(app): adds dark/light mode provider

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,10 +1,13 @@
 import { Button } from "@shadcn-ui/components/ui/button";
+import { ThemeProvider } from "./providers/theme";
 
 export const App = () => {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello World</h1>
-      <Button>Hello</Button>
+      <ThemeProvider defaultTheme="light" storageKey="thoughts-theme">
+        <h1 className="text-3xl font-bold underline">Hello World</h1>
+        <Button>Hello</Button>
+      </ThemeProvider>
     </>
   );
 };
