85f6e41c1b524c2372d5fde04e5f7f5bdb87c5d3
Fredrik Johansen <fredrik1206@gmail.com>
2024-05-07 20:46:28 +0200

    feat(app): adds signup page routing

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -2,12 +2,17 @@ import { ThemeProvider } from "./providers/theme";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import { HomePage } from "./routes/Home";
 import { LandingPage } from "./routes/Landing";
+import { SignupPage } from "./routes/Signup";
 
 export const App = () => {
   const router = createBrowserRouter([
     {
       path: "/",
       element: <HomePage />,
+    },
+    {
+      path: "/signup",
+      element: <SignupPage />,
     },
     {
       path: "/landing",
