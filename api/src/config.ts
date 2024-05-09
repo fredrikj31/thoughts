8ffbfa7fdb5a3626b41a9e74b286b90eaf526137
Fredrik Johansen <fredrik1206@gmail.com>
2024-05-09 09:38:16 +0200

    feat(api): adds website base url to config

--- api/src/config.ts
+++ api/src/config.ts
@@ -7,6 +7,7 @@ dotEnvConfig({ path: "../.env" });
 const envVarsSchema = z.object({
   API_HOST: z.string().default("0.0.0.0"),
   API_PORT: z.coerce.number().default(3000),
+  WEBSITE_BASE_URL: z.string(),
   DB_HOST: z.string(),
   DB_PORT: z.string(),
   DB_NAME: z.string(),
@@ -27,6 +28,9 @@ export const config = {
   api: {
     host: envVars.data.API_HOST,
     port: envVars.data.API_PORT,
+  },
+  website: {
+    baseUrl: envVars.data.WEBSITE_BASE_URL,
   },
   tokens: {
     jwtPrivateKey: envVars.data.JWT_PRIVATE_KEY,
