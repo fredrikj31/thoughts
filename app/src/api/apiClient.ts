2e093c5793ec1a680aeef00799bf8addac5b4150
Fredrik Johansen <fredrik1206@gmail.com>
2024-08-18 12:23:41 +0200

    feat(app): adds api auth interceptor to refresh token

--- app/src/api/apiClient.ts
+++ app/src/api/apiClient.ts
@@ -1,7 +1,10 @@
 import axios from "axios";
 import { config } from "../config";
+import { authInterceptor } from "./interceptor";
 
 export const apiClient = axios.create({
   baseURL: config.api.baseUrl,
   withCredentials: true,
 });
+
+apiClient.interceptors.request.use(authInterceptor);
