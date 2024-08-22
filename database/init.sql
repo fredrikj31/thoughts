8930842a0f33fc3a80a6867e84cebbf3b7d466db
Fredrik Johansen <fredrik1206@gmail.com>
2024-08-22 20:53:11 +0200

    feat(database): adds friends and friend requests table structure

--- database/init.sql
+++ database/init.sql
@@ -1,13 +1,20 @@
--- gender enum
+--- gender enum
 CREATE TYPE gender AS ENUM (
   'MALE',
   'FEMALE',
   'PREFER_NOT_TO_SAY'
 );
 
+--- friend request status enum
+CREATE TYPE friend_request_status AS ENUM (
+  'PENDING',
+  'ACCEPTED',
+  'DECLINED'
+);
+
 --- users table
 CREATE TABLE IF NOT EXISTS users (
-  id varchar(255) NOT NULL,
+  id uuid NOT NULL,
   username varchar(255) NOT NULL,
   email varchar(255) NOT NULL,
   password varchar(255) NOT NULL,
@@ -28,8 +35,8 @@ ALTER TABLE ONLY users ADD CONSTRAINT users_email_unique_key UNIQUE (email);
 
 --- refresh token table
 CREATE TABLE IF NOT EXISTS refresh_tokens (
-  id varchar(255) NOT NULL,
-  user_id varchar(255) NOT NULL,
+  id uuid NOT NULL,
+  user_id uuid NOT NULL,
   expires_at timestamp NOT NULL
 );
 
@@ -38,9 +45,33 @@ ALTER TABLE ONLY refresh_tokens ADD CONSTRAINT refresh_tokens_id_unique_key UNIQ
 ALTER TABLE ONLY refresh_tokens ADD CONSTRAINT refresh_tokens_user_id_references FOREIGN KEY(user_id) REFERENCES users(id);
 ---
 
--- posts table
-CREATE TABLE IF NOT EXISTS posts (
-  id varchar(255) NOT NULL UNIQUE PRIMARY KEY,
-  username varchar(255) NOT NULL,
-  email varchar(255) NOT NULL
+--- friends table
+CREATE TABLE IF NOT EXISTS friends (
+  id uuid NOT NULL,
+  user_id uuid NOT NULL,
+  friend_id uuid NOT NULL,
+  created_at timestamp NOT NULL,
+  updated_at timestamp,
+  deleted_at timestamp
 );
+
+ALTER TABLE ONLY friends ADD CONSTRAINT friends_id_primary_key PRIMARY KEY (id);
+ALTER TABLE ONLY friends ADD CONSTRAINT friends_user_id_references FOREIGN KEY(user_id) REFERENCES users(id);
+ALTER TABLE ONLY friends ADD CONSTRAINT friends_friend_id_references FOREIGN KEY(friend_id) REFERENCES users(id);
+---
+
+--- friend requests table
+CREATE TABLE IF NOT EXISTS friend_requests (
+  id uuid NOT NULL,
+  sender_id uuid NOT NULL,
+  receiver_id uuid NOT NULL,
+  status friend_request_status NOT NULL,
+  created_at timestamp NOT NULL,
+  updated_at timestamp,
+  deleted_at timestamp
+);
+
+ALTER TABLE ONLY friend_requests ADD CONSTRAINT friend_requests_id_primary_key PRIMARY KEY (id);
+ALTER TABLE ONLY friend_requests ADD CONSTRAINT friend_requests_sender_id_references FOREIGN KEY(sender_id) REFERENCES users(id);
+ALTER TABLE ONLY friend_requests ADD CONSTRAINT friend_requests_receiver_id_references FOREIGN KEY(receiver_id) REFERENCES users(id);
+---
