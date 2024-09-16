b086bbd4f1267b5499e26e038923a5131e4306e6
Fredrik Johansen <fredrik1206@gmail.com>
2024-09-16 20:57:00 +0200

    feat(app): adds friend requests schema and type

--- app/src/types/friend.ts
+++ app/src/types/friend.ts
@@ -10,3 +10,34 @@ export const FriendSchema = z.object({
   friend: UserSchema.omit({ password: true }),
 });
 export type Friend = z.infer<typeof FriendSchema>;
+
+const FriendRequestStatusSchema = z.enum(["PENDING", "ACCEPTED", "DECLINED"]);
+
+export const FriendRequestSchema = z.object({
+  id: z.string().uuid(),
+  receiverId: z.string().uuid(),
+  senderId: z.string().uuid(),
+  status: FriendRequestStatusSchema,
+  createdAt: z.string().datetime(),
+  updatedAt: z.string().datetime().nullable(),
+  deletedAt: z.string().datetime().nullable(),
+});
+export type FriendRequest = z.infer<typeof FriendRequestSchema>;
+
+export const ReceivedFriendRequestWithUserSchema = FriendRequestSchema.omit({
+  senderId: true,
+}).extend({
+  sender: UserSchema.omit({ password: true }),
+});
+export type ReceivedFriendRequestWithUser = z.infer<
+  typeof ReceivedFriendRequestWithUserSchema
+>;
+
+export const SentFriendRequestWithUserSchema = FriendRequestSchema.omit({
+  receiverId: true,
+}).extend({
+  receiver: UserSchema.omit({ password: true }),
+});
+export type SentFriendRequestWithUser = z.infer<
+  typeof SentFriendRequestWithUserSchema
+>;
