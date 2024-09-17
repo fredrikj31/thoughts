b43978f98f9a05c3f9b48dc6226d650ab6be6c41
Fredrik Johansen <fredrik1206@gmail.com>
2024-09-17 20:56:52 +0200

    feat(app): adds create post form logic on home page

--- app/src/routes/Home.tsx
+++ app/src/routes/Home.tsx
@@ -1,10 +1,78 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@shadcn-ui/components/ui/card";
 import { Navbar } from "../components/Navbar";
 import { Post } from "../components/Post";
+import { Textarea } from "@shadcn-ui/components/ui/textarea";
+import { Button } from "@shadcn-ui/components/ui/button";
+import { useState } from "react";
+import { useCreatePost } from "../api/posts/createPost/useCreatePost";
+import { useToast } from "@shadcn-ui/components/ui/use-toast";
 
 export const HomePage = () => {
+  const maxPostContentLength = 500;
+  const [content, setContent] = useState<string>("");
+  const { toast } = useToast();
+
+  const { mutate: createPost } = useCreatePost();
+
+  const createPostAction = () => {
+    if (!content || content.length === 0) {
+      toast({
+        title: "Empty thoughts?",
+        description: "Please fill out the form before posting",
+      });
+      return;
+    }
+
+    createPost(
+      { content },
+      {
+        onError: () => {
+          toast({
+            title: "Error :(",
+            description: "Error while posting your post",
+            variant: "destructive",
+          });
+        },
+        onSuccess: () => {
+          setContent("");
+        },
+      },
+    );
+  };
+
   return (
     <div className="flex flex-col mg:gap-7 gap-4 container">
       <Navbar />
+      {/* Create Post Form */}
+      <div className="flex self-center max-w-3xl w-full">
+        <Card className="w-full">
+          <CardHeader>
+            <CardTitle>Create Post</CardTitle>
+            <CardDescription>What's currently on your mind?</CardDescription>
+          </CardHeader>
+          <CardContent>
+            <Textarea
+              maxLength={maxPostContentLength}
+              placeholder="Lorem Ipsum..."
+              value={content}
+              onChange={(e) => setContent(e.currentTarget.value)}
+            />
+          </CardContent>
+          <CardFooter className="flex justify-between">
+            <span>
+              {content.split("").length} of {maxPostContentLength} characters
+            </span>
+            <Button onClick={createPostAction}>Post</Button>
+          </CardFooter>
+        </Card>
+      </div>
       {/* Feed */}
       <div className="flex flex-col self-center max-w-3xl gap-4">
         {/* Post */}
