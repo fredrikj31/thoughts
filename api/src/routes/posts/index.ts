88af789c1c94310c3fc3f13951a603b6b1d6ce67
Fredrik Johansen <fredrik1206@gmail.com>
2024-11-05 20:42:48 +0100

    feat(api): adds like/unlike post endpoint

--- api/src/routes/posts/index.ts
+++ api/src/routes/posts/index.ts
@@ -2,11 +2,17 @@ import { FastifyPluginAsync } from "fastify";
 import { ZodTypeProvider } from "fastify-type-provider-zod";
 import { validateJwt } from "../../hooks/validateJwt";
 import z from "zod";
-import { UnauthorizedError } from "../../errors/client";
+import { BadRequestError, UnauthorizedError } from "../../errors/client";
 import { createPostHandler } from "./handlers/createPost";
-import { PostSchema, PostWithUserSchema } from "../../types/post";
+import {
+  PostLikeSchema,
+  PostSchema,
+  PostWithUserSchema,
+} from "../../types/post";
 import { ApiErrorSchema } from "../../types/error";
 import { listPostsHandler } from "./handlers/listPosts";
+import { likePostHandler } from "./handlers/likePost";
+import { unlikePostHandler } from "./handlers/unlikePost";
 
 export const postsRoutes: FastifyPluginAsync = async (instance) => {
   const app = instance.withTypeProvider<ZodTypeProvider>();
@@ -93,6 +99,70 @@ export const postsRoutes: FastifyPluginAsync = async (instance) => {
 
       const posts = await listPostsHandler(database, { userId });
       return res.send(posts);
+    },
+  );
+
+  app.put(
+    "/:postId/likes",
+    {
+      onRequest: validateJwt(),
+      schema: {
+        summary: "Like/Unlike a specific post",
+        description: "Likes/Unlikes a specific post from a user.",
+        tags: ["posts"],
+        security: [
+          {
+            jwt: [""],
+          },
+        ],
+        params: z.object({
+          postId: z.string().uuid(),
+        }),
+        body: z.object({
+          status: z.enum(["like", "unlike"]),
+        }),
+        response: {
+          "200": PostLikeSchema.describe("Returns like/unlike item."),
+          "401": ApiErrorSchema.describe(
+            "Either is the access token missing, or the user id wasn't found in the access token provided.",
+          ),
+          "500": ApiErrorSchema.describe(
+            "Unknown server error, see logs for more details.",
+          ),
+        },
+      },
+    },
+    async (req, res) => {
+      const userId = req.user?.id;
+      if (!userId) {
+        throw new UnauthorizedError({
+          code: "user-id-not-found-in-request",
+          message: "A user id wasn't found in the request object",
+        });
+      }
+
+      const { postId } = req.params;
+      const { status } = req.body;
+
+      switch (status) {
+        case "like": {
+          const likedPost = await likePostHandler(database, { postId, userId });
+          return res.send(likedPost);
+        }
+        case "unlike": {
+          const unlikedPost = await unlikePostHandler(database, {
+            postId,
+            userId,
+          });
+          return res.send(unlikedPost);
+        }
+        default: {
+          throw new BadRequestError({
+            code: "invalid-status-type",
+            message: "The provided status is not valid",
+          });
+        }
+      }
     },
   );
 };
