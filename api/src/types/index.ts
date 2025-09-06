a2e25de20652cbcea1daa2f48069f990fb144cd3
Fredrik Johansen <fredrik1206@gmail.com>
2025-09-06 08:52:28 +0200

    fix(api): changes GenderSchema to a enum type

--- api/src/types/index.ts
+++ api/src/types/index.ts
@@ -6,8 +6,4 @@ export const DateSchema = z
     /^(?:19[0-9]{2}|2[0-1][0-9]{2})-(?:0[1-9]|1[0-2])-(?:0[1-9]|[1-2][0-9]|3[0-1])$/,
   );
 
-export const GenderSchema = z.union([
-  z.literal("MALE"),
-  z.literal("FEMALE"),
-  z.literal("PREFER_NOT_TO_SAY"),
-]);
+export const GenderSchema = z.enum(["MALE", "FEMALE", "PREFER_NOT_TO_SAY"]);
