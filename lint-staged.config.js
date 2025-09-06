d99eb5a8b37de680dcf653b9ec2080c022696df5
Fredrik Johansen <fredrik1206@gmail.com>
2025-09-06 10:30:36 +0200

    feat(lint-staged): adds "format:check" to api & app checks

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,11 +1,11 @@
 export default {
   "app/**/*": () => [
-    "pnpm run app:format",
+    "pnpm run app:format:check",
     "pnpm run app:lint",
     "pnpm run app:build:check",
   ],
   "api/**/*": () => [
-    "pnpm run api:format",
+    "pnpm run api:format:check",
     "pnpm run api:lint",
     "pnpm run api:build:check",
   ],
